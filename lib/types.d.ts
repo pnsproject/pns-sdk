6976da0fa69e4a2b2eda496794fce14ca5d8567c
jiangplus <jiang.plus.times@gmail.com>
2021-10-06 23:27:01 +0800

    empty code

--- lib/types.d.ts
+++
@@ -1,7 +0,0 @@
-/** Hex id */
-export declare type HexAddress = string;
-export declare type DomainString = string;
-export interface ContentType {
-    value: string;
-    contentType: string;
-}
--- src/types.ts
+++
@@ -1,9 +0,0 @@
-/** Hex id */
-export type HexAddress = string;
-
-export type DomainString = string;
-
-export interface ContentType {
-  value: string;
-  contentType: string;
-}
